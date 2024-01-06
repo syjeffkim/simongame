359fa3c964e40c346fe89dceacc95e34d31b2a52
syjeffkim <107138664+syjeffkim@users.noreply.github.com>
2024-01-06 10:53:48 -1000

    add sound to clicks.

--- game.js
+++ game.js
@@ -8,7 +8,6 @@ var gamePattern = [];
 var userClickedPattern = [];
 
 // handler function to detect when buttons are clicked with jQuery //
-
 $(".btn").on("click", function() {
 
     // creates variable for clicked btn //
@@ -16,11 +15,19 @@ $(".btn").on("click", function() {
 
     // pushes chosen color to array //
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+        // makes audio variable within Audio "constructor" //
+    
+    var audio = new Audio("sounds/" + userChosenColour + ".mp3");
 
+    // so within the Audio "constructor" there is a play function we are calling"
+    audio.play();
 });
 
-// generates random number to choose from buttonColours //
+// play sounds when you click button //
+
+
+
+// chooses next color in sequence and indicates with flash and sound //
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
 
