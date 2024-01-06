77dc996dbf79af5609f396fd4d8530ffd070f866
syjeffkim <107138664+syjeffkim@users.noreply.github.com>
2024-01-06 08:35:00 -1000

    move randomChosenColour and gamePattern inside nextSequence function. Variables made in functions are out of scope if you try call it from outside the function.

--- game.js
+++ game.js
@@ -1,7 +1,16 @@
+// array for color choices //
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+// array for randomly chosen colors //
+var gamePattern = [];
+
+// generates random number to choose from buttonColours //
 function nextSequence() {
-    var randomNumber = Math.floor((Math.random() * 3) + 1);
+    var randomNumber = Math.floor(Math.random() * 4);
     console.log(randomNumber);
+    
+    // chooses random color //
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 };
-
-
 
