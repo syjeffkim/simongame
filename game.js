ed5cec613ab8609d8e430e73710a2d8d0005393d
syjeffkim <107138664+syjeffkim@users.noreply.github.com>
2024-01-06 16:31:28 -1000

    remove quotes from document in jQuery target so it looks at document and not a syntax.

--- game.js
+++ game.js
@@ -7,6 +7,28 @@ var gamePattern = [];
 // array to track user clicks //
 var userClickedPattern = [];
 
+// You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress. //
+
+var started = false;
+
+// to set levels //
+
+var level = 0;
+
+// 1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence(). //
+
+$(document).keydown(function() {
+
+
+        //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    
+});
+
+
+
 // handler function to detect when buttons are clicked with jQuery //
 $(".btn").on("click", function() {
 
@@ -30,6 +52,13 @@ $(".btn").on("click", function() {
 
 // chooses next color in sequence and indicates with flash and sound //
 function nextSequence() {
+    //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called. //
+    level++;
+
+    //5. Inside nextSequence(), update the h1 with this change in the value of level. //
+    $("#level-title").text("Level " + level);
+
+    // chooses random number 0-3 //
     var randomNumber = Math.floor(Math.random() * 4);
 
     // chooses random color //
@@ -43,6 +72,7 @@ function nextSequence() {
 
     // plays sound of randomized color //
     playSound(randomChosenColour);
+    console.log("hi");
 };
 
 function playSound(name) {
@@ -67,5 +97,4 @@ function animatePress(currentColour) {
       }, 100);
 
 
-}
-
+};
